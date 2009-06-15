bfb6c38ba807555d64c08f0e1454e069060e2967
Tom Parker <palfrey@tevp.net>
2009-06-15 15:35:44 +0100

    Finished dorkbot code

--- dorkbot.php
+++ dorkbot.php
@@ -18,55 +18,75 @@
  * License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once('lastRSS.php');
 require_once( 'iCalcreator.class.php' );
+require_once( 'tz.php' );
+
 $v = new vcalendar(); // create a new calendar instance
-$v->parse("dorkbot.ics");
-$lr = new lastRSS();
-$data = $lr->Get("dorkbot.rss");
 
-$mapping = array();
+$data = get ("http://dorkbotlondon.org/feeds/icalendar/");
+$v->parseString($data);
 
-foreach($data['items'] as $item)
+$out = new vcalendar();
+$out->setXprop("X-WR-CALNAME","Dorkbot London events");
+$out->setConfig( 'unique_id', 'tevp.net' ); // set Your unique id, required if property UID is missing
+$out->setProperty( 'method', 'PUBLISH' ); // required of some calendar software
+
+function get($url, $max_age = 3600)
 {
-	$link = $item['link'];
-	$lines = explode("\n",html_entity_decode($item['description']));
-	$inloc = False;
-	$loc = "";
-	foreach($lines as $line)
+	if (!file_exists("cache"))
+		mkdir("cache");
+	$fname = "cache/".md5($url);
+	$age = filemtime($fname);
+	if (!$age || ($max_age != -1 && time()-$age > $max_age))
 	{
-		if ($line == "<h2>Location</h2>")
-		{
-			$inloc = True;
-		}
-		else if ($inloc && $line == "<br />")
-		{
-			$inloc = False;
-		}
-		else if ($inloc)
-		{
-			$l = trim(strip_tags($line));
-			if ($l == "")
-				continue;
-			if ($l[-1] != ",")
-				$l .= ",";
-			$l .= " ";
-			$loc .= $l;
-		}
+		$ret = file_get_contents($url);
+		file_put_contents($fname, $ret);
 	}
-	$loc = substr($loc, 0, -2);
-	#print "Location: $loc\n";
-	$mapping[$link] = urlencode($loc);
-}
-while( $vevent = $v->getComponent( 'vevent' )) {
-	$loc = $mapping[$vevent->getProperty("SUMMARY")];
-	if ($loc!= "")
-	{
-		print "loc: $loc\n";
-		$vevent->setProperty( 'LOCATION', $loc);
-		$v->addComponent($vevent);
-	}
+	else
+		$ret = file_get_contents($fname);
+	return $ret;
 }
 
-print $v->createCalendar();
+$timezone = "Europe/London";
+$tz = genTimezone ($timezone);
+$tzid = $tz->getProperty("TZID");
+$out->addComponent($tz);
+
+while( $vevent = $v->getComponent( 'vevent' )) {
+	$url = $vevent->getProperty("SUMMARY");
+	$data = get($url, -1);
+	if ($data != "")
+	{
+		preg_match("/(<h2>.*?)<h2>/s", $data, $matches);
+		$data = $matches[1];
+		$lines = explode("\n", $data, 2);
+		$title = strip_tags($lines[0]);
+		#print "Title: $title\n";
+		$vevent->setProperty("SUMMARY", $title);
+
+		preg_match("/When:<\/dt><dd>([^<]+)<\/dd>/", $data, $matches);
+		$when = explode(",", $matches[1]);
+		$time = strptime($when[1], "%e %B %Y");
+		$times = explode("-",$when[0]);
+
+		$start = $vevent->getProperty('DTSTART');
+		$start['tz'] = $tzid;
+		$vevent->setProperty('DTSTART',$start);
+
+		$end = $vevent->getProperty('DTEND');
+		$end['tz'] = $tzid;
+		$vevent->setProperty('DTEND',$end);
+
+		$content = trim(substr(strstr($data, "</dl>"), 5));
+		#print $content."\n";
+		$vevent->setProperty('description',$content);
+	}
+	$loc = $vevent->getProperty("location");
+	if ($loc == "Limehouse town hall")
+		$vevent->setProperty("location","Limehouse Town Hall, 646 Commercial Road London, E14 7HA, United Kingdom");
+
+	$out->addComponent($vevent);
+}
+
+print $out->createCalendar();
 ?>
